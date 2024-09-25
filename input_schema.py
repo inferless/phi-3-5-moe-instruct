77346ae46c5159a66184d0c36e9a4d9c085be7b7
Nilesh Agarwal <nick.aggarwal@gmail.com>
2024-09-26 04:03:54 +0530

    Create input_schema.py

---
+++ input_schema.py
@@ -0,0 +1,8 @@
+INPUT_SCHEMA = {
+    "prompt": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["Can you provide ways to eat combinations of bananas and dragonfruits?"]
+    }
+}
